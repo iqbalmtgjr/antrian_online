53f882c356666810d2d26386f0313d80e68fd130
iqbalmtgjr <iqbalmtgjr@gmail.com.com>
2021-03-04 19:50:36 +0700

    belum dinamis antriannya

--- app/Http/Controllers/AntrianController.php
+++ app/Http/Controllers/AntrianController.php
@@ -124,16 +124,16 @@ class AntrianController extends Controller
                 'no_antrian' => $antri,
                 'estimasi' => date('H:i:s', strtotime($last->estimasi) + ($lama_pelayanan * 60))
             ]);
-            // $last = Antrian::where('id_pelayanan', 1)->get()->last();
-            // $last_count = $last->no_antrian - 1;
-            // $antriann = Antrian::where('no_antrian', $last_count)->where('id_pelayanan', 1)->first();
-            // $waktu_awal = new DateTime($antriann->waktu_awal_antrian);
-            // $waktu_akhir = new DateTime($antriann->waktu_akhir_antrian);
-            // $hitung = $waktu_awal->diff($waktu_akhir);
-            // $antriann->update([
-            //     'waktu_akhir_antrian' => $last->waktu_awal_antrian,
-            //     // 'lama_pelayanan' => $hitung->format('%H:%I:%S')
-            // ]);
+            $last = Antrian::where('id_pelayanan', 1)->get()->last();
+            $last_count = $last->no_antrian - 1;
+            $antriann = Antrian::where('no_antrian', $last_count)->where('id_pelayanan', 1)->first();
+            $waktu_awal = new DateTime($antriann->waktu_awal_antrian);
+            $waktu_akhir = new DateTime($antriann->waktu_akhir_antrian);
+            $hitung = $waktu_awal->diff($waktu_akhir);
+            $antriann->update([
+                'waktu_akhir_antrian' => $last->waktu_awal_antrian,
+                'lama_pelayanan' => $hitung->format('%H:%I:%S')
+            ]);
             // dd($antriann);
         }
 
@@ -480,33 +480,32 @@ class AntrianController extends Controller
         $antriann = Antrian::where('no_antrian', $first_count)->where('id_pelayanan', 1)->first();
         $waktu_awal = new DateTime($antrian->waktu_awal_antrian);
         $waktu_akhir = new DateTime($antriann->waktu_awal_antrian);
-        $hitung = $waktu_awal->diff($waktu_akhir);
+        $lama_pelayanan = $waktu_awal->diff($waktu_akhir);
         $estimasi = new DateTime($antrian->estimasi);
-        $kurang = new DateTime($hitung->format('%H:%I:%S'));
-        if ($antrian->estimasi > $localtime) {
-            $laporan = Laporan::create([
-                'id_pelayanan' => $antrian->id_pelayanan,
-                'id_user' => $antrian->id_user,
-                'no_antrian' => $antrian->no_antrian,
-                'hari' => $antrian->hari,
-                'tgl_antrian' => $antrian->tgl_antrian,
-                'waktu_awal_antrian' => $antrian->waktu_awal_antrian,
-                'waktu_akhir_antrian' => $antriann->waktu_awal_antrian,
-                'lama_pelayanan' => $hitung->format('%H:%I:%S'),
-                'estimasi' => ($estimasi)->diff($kurang)->format('%H:%I:%S'),
-                // 'estimasi' => date('H:i:s', strtotime($antrian->estimasi) - strtotime($hitung->format('%H:%I:%S')))
-            ]);
-        } else {
-            $laporan = Laporan::create([
-                'id_pelayanan' => $antrian->id_pelayanan,
-                'id_user' => $antrian->id_user,
-                'no_antrian' => $antrian->no_antrian,
-                'hari' => $antrian->hari,
-                'tgl_antrian' => $antrian->tgl_antrian,
-                'waktu_awal_antrian' => $antrian->waktu_awal_antrian,
-                'waktu_akhir_antrian' => $antriann->waktu_awal_antrian,
-                'lama_pelayanan' => $hitung->format('%H:%I:%S'),
-                'estimasi' => $antrian->estimasi
+        $kurang = new DateTime($lama_pelayanan->format('%H:%I:%S'));
+        $antrian_all = Antrian::where('id_pelayanan', 1)->get();
+        $laporan = Laporan::create([
+            'id_pelayanan' => $antrian->id_pelayanan,
+            'id_user' => $antrian->id_user,
+            'no_antrian' => $antrian->no_antrian,
+            'hari' => $antrian->hari,
+            'tgl_antrian' => $antrian->tgl_antrian,
+            'waktu_awal_antrian' => $antrian->waktu_awal_antrian,
+            'waktu_akhir_antrian' => $antriann->waktu_awal_antrian,
+            'lama_pelayanan' => $lama_pelayanan->format('%H:%I:%S'),
+            'estimasi' => ($estimasi)->diff($kurang)->format('%H:%I:%S'),
+        ]);
+        foreach ($antrian_all as $ann) {
+            $laporann = Laporan::where('id_pelayanan', 1)->get();
+            foreach ($laporann as $lap) {
+                # code...
+                $w_akhir = new DateTime($lap->lama_pelayanan);
+                dd($lap->lama_pelayanan);
+            }
+            $w_awal = new DateTime($ann->waktu_awal_antrian);
+            $l_pelayanan = $w_awal->diff($w_akhir)->format('%H:%I:%S');
+            $update = Antrian::where('id_pelayanan', 1)->update([
+                'estimasi' => (new DateTime($ann->estimasi))->diff(new DateTime($l_pelayanan))->format('%H:%I:%S'),
             ]);
         }
 
